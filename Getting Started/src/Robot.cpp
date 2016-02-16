edb800eb68f2191202020359e109d230ff88ce85
Grady Crossley <mathmaniac@netptc.net>
2016-02-15 16:36:47 -0800

    Removed older code, since pneumatics solution has been simplified

--- Getting Started/src/Robot.cpp
+++ Getting Started/src/Robot.cpp
@@ -7,29 +7,27 @@ class Robot: public IterativeRobot
 	RobotDrive myRobot; // robot drive system
 	Talon shooter;
 	DigitalInput shooter_stop;
-	Joystick stick; // only joystick
+	Joystick stick1, stick2, gamepad; // only joystick
 	LiveWindow *lw;
 	int autoLoopCounter;
 	DoubleSolenoid solenoid1;
-	DoubleSolenoid solenoid2;
-	DoubleSolenoid solenoid3;
 	Compressor *c = new Compressor(0);
 	Timer time_since, auto_time, shoot_time;
 	// FIX ME: Pointers for the following variables are not working
-	bool x_btn() { return stick.GetRawButton(1); }
-	bool a_btn() { return stick.GetRawButton(2); }
-	bool b_btn() { return stick.GetRawButton(3); }
-	bool y_btn() { return stick.GetRawButton(4); }
-	bool lb() { return stick.GetRawButton(5); }
-	bool rb() { return stick.GetRawButton(6); }
-	bool lt() { return stick.GetRawButton(7); }
-	bool rt() { return stick.GetRawButton(8); }
-	bool back_btn() { return stick.GetRawButton(9); }
-	bool start_btn() { return stick.GetRawButton(10); }
-	float lstick_x() { return stick.GetRawAxis(0); }
-	float lstick_y() { return 0.9*stick.GetRawAxis(1); }
-	float rstick_x() { return stick.GetRawAxis(2); }
-	float rstick_y() { return 0.9*stick.GetRawAxis(3); }
+	bool x_btn() { return gamepad.GetRawButton(1); }
+	bool a_btn() { return gamepad.GetRawButton(2); }
+	bool b_btn() { return gamepad.GetRawButton(3); }
+	bool y_btn() { return gamepad.GetRawButton(4); }
+	bool lb() { return gamepad.GetRawButton(5); }
+	bool rb() { return gamepad.GetRawButton(6); }
+	bool lt() { return gamepad.GetRawButton(7); }
+	bool rt() { return gamepad.GetRawButton(8); }
+	bool back_btn() { return gamepad.GetRawButton(9); }
+	bool start_btn() { return gamepad.GetRawButton(10); }
+	float lstick_x() { return stick1.GetRawAxis(0); }
+	float lstick_y() { return stick1.GetRawAxis(1); }
+	float rstick_x() { return stick2.GetRawAxis(0); }
+	float rstick_y() { return stick2.GetRawAxis(1); }
 
 	void raise(){
 		// pistons out
@@ -57,12 +55,12 @@ public:
 		myRobot(0, 1),	// these must be initialized in the same order
 		shooter(3),
 		shooter_stop(0),
-		stick(0),		// as they are declared above.
+		stick1(0),
+		stick2(1),
+		gamepad(2),// as they are declared above.
 		lw(LiveWindow::GetInstance()),
 		autoLoopCounter(0),
-		solenoid1(2, 3),
-		solenoid2(7, 6),
-		solenoid3(0, 1)  //forward turns on first port
+		solenoid1(2, 3)
 	{
 		myRobot.SetExpiration(0.1);
 	}
@@ -76,6 +74,7 @@ private:
 	void AutonomousInit()
 	{
 		autoLoopCounter = 0;
+		c->SetClosedLoopControl(true);
 	}
 
 	void AutonomousPeriodic()
@@ -105,55 +104,15 @@ private:
 			myRobot.Drive(0.0, 0.0);
 			shooter.Set(0.0);                     //stop all motors
 		}
-
-		/*if(autoLoopCounter < 50) //Check if we've completed 100 loops (approximately 2 seconds)
-		{
-			myRobot.Drive(-0.5, 0.0); 	// drive forwards half speed
-//			shooter.SetInverted(true);
-			shooter.Set(-0.5);
-			std::stringstream ss;
-			std::string a;
-			ss<<shooter.Get();
-			ss>>a;
-			SmartDashboard::PutString("DB/String 0", a);
-			autoLoopCounter++;
-		}
-		else if(autoLoopCounter < 100) {
-			// wait
-			shooter.Set(0.0);
-			autoLoopCounter++;
-		}
-		else if(autoLoopCounter < 150) {
-			shooter.Set(1.0);
-			autoLoopCounter++;
-		}
-		else {
-			myRobot.Drive(0.0, 0.0); 	// stop robot
-			shooter.Set(0.0);
-		}*/
 	}
 
 	void TeleopInit()
 	{
+		c->SetClosedLoopControl(true);
 	}
 
 	void TeleopPeriodic()
 	{
-		c->SetClosedLoopControl(true);
-
-		//myRobot.ArcadeDrive(stick);  drive with arcade style (use right stick)
-//		shooter.SetInverted(true);
-
-//		if(stick.GetRawButton(1) && !shooter_stop.Get()){
-//			shooter.Set(-0.5);    //shooter pulls in if limit switch not pressed and x button pressed
-//		}
-//		else if(stick.GetRawButton(4)) {
-//			shooter.Set(1);    //shooter shoots if y button pressed
-//		}
-//		else {
-//			shooter.Set(0);
-//		}
-
 		//This is that part where we summon an alien mothership to control our robot for us
 		myRobot.TankDrive(lstick_y(),rstick_y());
 
@@ -186,27 +145,11 @@ private:
 
 			raise();
 
-			// pistons in
-			//solenoid1.Set(DoubleSolenoid::Value::kForward);
-		}else if(a_btn()){
+		} else if(a_btn()){
 
 			lower();
 
-			// pistons out
-			//solenoid1.Set(DoubleSolenoid::Value::kReverse);
 		}
-
-//		checkRaise(2);
-
-		/*if(rb()){
-			// switch to 60 psi
-			solenoid2.Set(DoubleSolenoid::Value::kForward);
-			solenoid3.Set(DoubleSolenoid::Value::kReverse);
-		}else if(lb()){
-			// switch to 30 psi (or other)
-			solenoid2.Set(DoubleSolenoid::Value::kReverse);
-			solenoid3.Set(DoubleSolenoid::Value::kForward);
-		} */
 
 	}
 
